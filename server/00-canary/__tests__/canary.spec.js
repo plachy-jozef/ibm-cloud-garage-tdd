fbdb3a4209eabf227bd266d92cdaa6251d639c15
jozef.plachy <jozef.plachy@sk.ibm.com>
2019-09-10 07:47:38 +0200

    passing 3rd test

--- server/00-canary/__tests__/canary.spec.js
+++ server/00-canary/__tests__/canary.spec.js
@@ -12,8 +12,8 @@ describe('the canary spec', () => {
 		it ('fahrenheit2celsius of 212 is 100', () => {
 			expect(fahrenheit2celsius(212)).toEqual(100);
 		});
-		it ('fahrenheit2celsius of 50 is 0', () => {
-			
+		it ('fahrenheit2celsius of 50 is 10', () => {
+			expect(fahrenheit2celsius(50)).toEqual(10);			
 		});
 	});
 });
