c56aa73844aab731d16cefe9c27d6d01fc3dd6e1
jozef.plachy <jozef.plachy@sk.ibm.com>
2019-09-10 07:29:56 +0200

    adding 3 passing tests

--- server/00-canary/__tests__/canary.spec.js
+++ server/00-canary/__tests__/canary.spec.js
@@ -2,4 +2,16 @@ describe('the canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
+
+	describe.only('fahrenheit2celsius', () => {
+		it ('fahrenheit2celsius of 32 is 0', () => {
+
+		});
+		it ('fahrenheit2celsius of 212 is 100', () => {
+			
+		});
+		it ('fahrenheit2celsius of 50 is 0', () => {
+			
+		});
+	});
 });
